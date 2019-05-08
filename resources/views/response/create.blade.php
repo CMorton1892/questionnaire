4f95997b76e457a2c595f33f3dd7948edf0f85b0
Cameron Morton <23604115@edgehill.ac.uk>
2019-05-08 06:38:45 +0100

    Views updated

--- resources/views/response/create.blade.php
+++ resources/views/response/create.blade.php
@@ -13,14 +13,14 @@
   <div class="container">
     <article>
       <h1>Answer the Following Questions</h1>
-      <h4>Please answer each question.</h4>
+      <h4>How strongly do you agree with the following statements?</h4>
       <hr>
       <form action="/response" method="post">
           {{ csrf_field() }}
           <div class="form-group">
               <!-- Should pull question from questions model -->
               <!-- Likert scale used to generate responses -->
-              <label for="title">Question 1</label>
+              <label for="title">It is important to focus on the target audience when creating questions for a questionnaire.</label><br/>
               <label class="radio-inline"><input type="radio" name="response1" value="1">   Strongly Agree</label>
               <label class="radio-inline"><input type="radio" name="response1" value="2">   Agree</label>
               <label class="radio-inline"><input type="radio" name="response1" value="3">   Neither</label>
@@ -30,7 +30,7 @@
           <div class="form-group">
               <!-- Should pull question from questions model -->
               <!-- Likert scale used to generate responses -->
-              <label for="title">Question 2</label>
+              <label for="title">Respondents are more likely to provide answers which presents them in a good light if they feel that their true answer would be deemed as controversial or unpopular.</label><br/>
               <label class="radio-inline"><input type="radio" name="response2" value="1">   Strongly Agree</label>
               <label class="radio-inline"><input type="radio" name="response2" value="2">   Agree</label>
               <label class="radio-inline"><input type="radio" name="response2" value="3">   Neither</label>
@@ -40,7 +40,7 @@
           <div class="form-group">
               <!-- Should pull question from questions model -->
               <!-- Likert scale used to generate responses -->
-              <label for="title">Question 3</label>
+              <label for="title">Respondents become disinterested or confused when the questions which form a questionnaire are not in a particular order or do not flow smoothly from one another.</label><br/>
               <label class="radio-inline"><input type="radio" name="response3" value="1">   Strongly Agree</label>
               <label class="radio-inline"><input type="radio" name="response3" value="2">   Agree</label>
               <label class="radio-inline"><input type="radio" name="response3" value="3">   Neither</label>
@@ -50,7 +50,7 @@
           <div class="form-group">
               <!-- Should pull question from questions model -->
               <!-- Likert scale used to generate responses -->
-              <label for="title">Question 4</label>
+              <label for="title">It is important for the respondents to know the purpose behind the questionnaire and why they are completing it.</label><br/>
               <label class="radio-inline"><input type="radio" name="response4" value="1">   Strongly Agree</label>
               <label class="radio-inline"><input type="radio" name="response4" value="2">   Agree</label>
               <label class="radio-inline"><input type="radio" name="response4" value="3">   Neither</label>
@@ -60,7 +60,7 @@
           <div class="form-group">
               <!-- Should pull question from questions model -->
               <!-- Likert scale used to generate responses -->
-              <label for="title">Question 5</label>
+              <label for="title">Respondents are less likely to complete a questionnaire to their true ability or thoughts if they have little interest in the subject of the questions.</label><br/>
               <label class="radio-inline"><input type="radio" name="response5" value="1">   Strongly Agree</label>
               <label class="radio-inline"><input type="radio" name="response5" value="2">   Agree</label>
               <label class="radio-inline"><input type="radio" name="response5" value="3">   Neither</label>
